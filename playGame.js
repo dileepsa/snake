284f8688bb7274cfad0d3fe2bf2f94fe065e7512
Dileep A <99489054+dileepsa@users.noreply.github.com>
2022-06-11 15:46:55 +0530

    Extracted snake into a seperate file.

---
+++ playGame.js
@@ -0,0 +1,58 @@
+const { Box } = require('./box.js');
+const { Snake } = require('./snake.js');
+const { stdout, stdin } = require('process');
+const EventEmitter = require('events');
+const HIDDEN_CURSOR = '\x1B[?25l';
+
+const resetScreen = () => {
+  stdout.cursorTo(70, 10);
+};
+
+const setupScreen = () => {
+  stdout.cursorTo(0, 0);
+  stdout.write(HIDDEN_CURSOR);
+  stdout.clearScreenDown();
+};
+
+const setEvents = (snake) => {
+  const upKey = 'w';
+  const leftKey = 'a';
+  const downKey = 's';
+  const rightKey = 'd'
+  const events = new EventEmitter();
+
+  events.on(leftKey, () => snake.move(-1, 0));
+  events.on(rightKey, () => snake.move(1, 0));
+  events.on(upKey, () => snake.move(0, -1));
+  events.on(downKey, () => snake.move(0, 1));
+  return events
+};
+
+const playGame = (events, key, snake, box) => {
+  snake.eraseSnake();
+  events.emit(key.toString());
+  if (snake.isTouched(box)) {
+    stdout.write('You Died!!!!!!!!!!!!!!!!');
+    resetScreen();
+    process.exit(0);
+  }
+  snake.drawSnake();
+};
+
+const main = () => {
+
+  setupScreen();
+  const box = new Box(20, 30);
+  box.drawBox();
+  const snake = new Snake(51, 11);
+  snake.drawSnake();
+  const events = setEvents(snake);
+
+  stdin.setRawMode(true);
+
+  stdin.on('data', (key) => {
+    playGame(events, key, snake, box);
+  })
+};
+
+main();
